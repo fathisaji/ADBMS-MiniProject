34eb831b27b72badd44ae0faa183eb630b644176
Fathima Sajeeya Mohamed Roshan <123949626+fathisaji@users.noreply.github.com>
2025-10-28 15:41:52 +0530

    Delete vehicle_rental_backend directory

--- vehicle_rental_backend/src/main/java/com/vehiclerental/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vehiclerental.repository;
-
-
-
-import com.vehiclerental.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
